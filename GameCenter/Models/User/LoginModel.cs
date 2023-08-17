b68b5ecf96787bf78c6508687705bcf0f7b0e174
Remigiusz Bekierz <103840769+Remulus1921@users.noreply.github.com>
2023-08-17 16:36:00 +0200

    User Models

---
+++ GameCenter/Models/User/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameCenter.Models.User
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
---
+++ GameCenter/Models/User/UserRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace GameCenter.Models.User
+{
+    public class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string Moderator = "Moderator";
+        public const string User = "User";
+    }
+}
