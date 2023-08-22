646ef07392b2d6b60e20c97f390b4737d42b7827
Remigiusz Bekierz <103840769+Remulus1921@users.noreply.github.com>
2023-08-22 13:53:52 +0200

    Created CRUD operations for Platform Entity

---
+++ GameCenter/Controllers/PlatformController.cs
@@ -0,0 +1,68 @@
+﻿using GameCenter.Core.Services.PlatformService;
+using GameCenter.Dtos.PlatformDto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameCenter.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PlatformController : ControllerBase
+    {
+        private readonly IPlatformService _platformService;
+
+        public PlatformController(IPlatformService platformService)
+        {
+            _platformService = platformService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPlatforms()
+        {
+            var result = await _platformService.GetPlatforms();
+
+            if (!result.Any())
+            {
+                return NoContent();
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPlatform([FromBody] PlatformDto platformDto)
+        {
+            var result = await _platformService.AddPlatform(platformDto);
+
+            if (!result)
+            {
+                return BadRequest("Platform with given name already exists");
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeletePlatform([FromBody] PlatformDto platformDto)
+        {
+            var result = await _platformService.DeletePlatform(platformDto);
+            if (!result)
+            {
+                return BadRequest("There is no platform with provided name");
+            }
+
+            return Ok();
+        }
+
+        [HttpPut("/{newName}")]
+        public async Task<IActionResult> UpdatePlatform([FromBody] PlatformDto platformDto, [FromRoute] string newName)
+        {
+            var result = await _platformService.UpdatePlatform(platformDto, newName);
+            if (!result)
+            {
+                return BadRequest("No platform");
+            }
+
+            return Ok();
+        }
+    }
+}
