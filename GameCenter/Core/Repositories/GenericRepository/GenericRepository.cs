9659046d6a9ccd3c0a244b80ed24dd882d335b25
Remigiusz Bekierz <103840769+Remulus1921@users.noreply.github.com>
2023-08-22 12:15:14 +0200

    Created generic repository

---
+++ GameCenter/Core/Repositories/GenericRepository/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using GameCenter.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameCenter.Core.Repositories.GenericRepository;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : class
+{
+    protected ApplicationDbContext _context;
+    internal DbSet<T> _dbSet;
+
+    public GenericRepository(ApplicationDbContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<T>();
+    }
+
+    public virtual async Task<bool> Add(T entity)
+    {
+        _dbSet.Add(entity);
+        return true;
+    }
+
+    public virtual async Task<IEnumerable<T>> All()
+    {
+        return await _dbSet.AsNoTracking().ToListAsync();
+    }
+
+    public virtual async Task<bool> Delete(T entity)
+    {
+        _dbSet.Remove(entity);
+        return true;
+    }
+
+    public virtual async Task<T?> GetById(int id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public virtual async Task<bool> Update(T entity)
+    {
+        _dbSet.Update(entity);
+        return true;
+    }
+}
---
+++ GameCenter/Core/Repositories/GenericRepository/IGenericRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace GameCenter.Core.Repositories.GenericRepository;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<IEnumerable<T>> All();
+    Task<T?> GetById(int id);
+    Task<bool> Add(T entity);
+    Task<bool> Update(T entity);
+    Task<bool> Delete(T entity);
+}
