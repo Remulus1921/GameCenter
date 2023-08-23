ea0742721ae63966b0913ec76311be445979e270
Remigiusz Bekierz <103840769+Remulus1921@users.noreply.github.com>
2023-08-23 11:51:28 +0200

    Dtos for game entity added

---
+++ GameCenter/Dtos/GameDto/GameSmallDto.cs
@@ -0,0 +1,11 @@
+﻿namespace GameCenter.Dtos.GameDto
+{
+    public class GameSmallDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string GameType { get; set; }
+        public string Rating { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
