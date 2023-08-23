23d4343fc993cc4cc341f61b3f7d9248a82d4286
Remigiusz Bekierz <103840769+Remulus1921@users.noreply.github.com>
2023-08-23 15:54:09 +0200

    Claim change from Name to Email in Authorization

--- GameCenter/Core/Services/AuthService/AuthService.cs
+++ GameCenter/Core/Services/AuthService/AuthService.cs
@@ -135,7 +135,7 @@ public class AuthService : IAuthService
         var userRoles = await _userManager.GetRolesAsync(user);
         var authClaims = new List<Claim>
         {
-            new Claim(ClaimTypes.Name, user.UserName!),
+            new Claim(ClaimTypes.Email, user.Email!),
             new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
         };
 
