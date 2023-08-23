b07dcacc557ad7797bd369301855983bbdaf5bb6
Remigiusz Bekierz <103840769+Remulus1921@users.noreply.github.com>
2023-08-23 11:48:47 +0200

    Repository for games added

---
+++ GameCenter/Core/Repositories/GameRepository/GamesRepository.cs
@@ -0,0 +1,12 @@
+﻿using GameCenter.Core.Repositories.GenericRepository;
+using GameCenter.Data;
+using GameCenter.Models;
+
+namespace GameCenter.Core.Repositories.GameRepository;
+
+public class GamesRepository : GenericRepository<Game>, IGamesRepository
+{
+    public GamesRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
---
+++ GameCenter/Core/Repositories/GameRepository/IGamesRepository.cs
@@ -0,0 +1,8 @@
+﻿using GameCenter.Core.Repositories.GenericRepository;
+using GameCenter.Models;
+
+namespace GameCenter.Core.Repositories.GameRepository;
+
+public interface IGamesRepository : IGenericRepository<Game>
+{
+}
