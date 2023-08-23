c8494ee1693dd6f5c59b6f83333dd812d30cb4b7
Remigiusz Bekierz <103840769+Remulus1921@users.noreply.github.com>
2023-08-23 11:48:29 +0200

    GamesController added

---
+++ GameCenter/Controllers/GamesController.cs
@@ -0,0 +1,55 @@
+﻿using GameCenter.Core.Services.GameService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameCenter.Controllers;
+
+// TODO POST, PUT
+
+[Route("[controller]")]
+[ApiController]
+public class GamesController : ControllerBase
+{
+    private readonly IGamesService _gamesService;
+
+    public GamesController(IGamesService gamesService)
+    {
+        _gamesService = gamesService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetGames()
+    {
+        var result = await _gamesService.GetGames();
+
+        if (result == null)
+        {
+            return NoContent();
+        }
+
+        return Ok(result);
+    }
+
+    [HttpGet("/{gameId}")]
+    public async Task<IActionResult> GetGameById([FromRoute] Guid gameId)
+    {
+        var result = await _gamesService.GetGameById(gameId);
+        if (result == null)
+        {
+            return BadRequest("There is no game witch id = " + gameId);
+        }
+
+        return Ok(result);
+    }
+
+    [HttpDelete("/{gameId}")]
+    public async Task<IActionResult> DeleteGame([FromRoute] Guid gameId)
+    {
+        var result = await _gamesService.DeleteGame(gameId);
+        if (result == false)
+        {
+            return NotFound();
+        }
+
+        return Ok("Game successfully deleted");
+    }
+}
