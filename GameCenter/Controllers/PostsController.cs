22c5aa0721c4f9e0146584290110d9fefade8967
Remigiusz Bekierz <103840769+Remulus1921@users.noreply.github.com>
2023-08-31 10:24:58 +0200

    Endpoints for Posts Table added

---
+++ GameCenter/Controllers/PostsController.cs
@@ -0,0 +1,89 @@
+﻿using GameCenter.Core.Services.PostsService;
+using GameCenter.Dtos.PostDto;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace GameCenter.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PostsController : ControllerBase
+{
+    private readonly IPostsService _postsService;
+
+    public PostsController(IPostsService postsService)
+    {
+        _postsService = postsService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetPosts()
+    {
+        var result = await _postsService.GetPosts();
+
+        if (result == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(result);
+    }
+
+    [HttpGet("{postId}")]
+    public async Task<IActionResult> GetPostById([FromRoute] Guid postId)
+    {
+        var result = await _postsService.GetPost(postId);
+
+        if (result == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(result);
+    }
+
+    [HttpDelete("{postId}")]
+    public async Task<IActionResult> RemovePost([FromRoute] Guid postId)
+    {
+        var result = await _postsService.RemovePost(postId);
+        if (!result)
+        {
+            return NotFound();
+        }
+
+        return NoContent();
+    }
+
+    [HttpPut("{postId}")]
+    public async Task<IActionResult> UpdatePost([FromRoute] Guid postId, [FromBody] PostAddUpdateDto post)
+    {
+        var result = await _postsService.UpdatePost(postId, post);
+
+        if (!result)
+        {
+            return BadRequest();
+        }
+
+        return Ok();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddPost([FromBody] PostAddUpdateDto post)
+    {
+        var emailClaim = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email);
+        if (emailClaim == null)
+        {
+            return NotFound("No email claim in authorization");
+        }
+        string email = emailClaim.Value;
+
+        var result = await _postsService.AddPost(post, email);
+
+        if (!result)
+        {
+            return NotFound("No User with provided email");
+        }
+
+        return Ok("Post Successfully added");
+    }
+}
